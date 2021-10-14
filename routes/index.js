6c342572ec297d959ffd5ffb77e56112b3bda94b
pratik-chandrasekhar-pattanaik <chandrasekhar.pcp@gmail.com>
2021-10-14 14:16:22 +0530

    comment-intro

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ console.log('router-1 is loaded');
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 module.exports = router;
