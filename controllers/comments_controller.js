83f38216f43d71f2be4b9734fca8dce439ab48cc
pratik-chandrasekhar-pattanaik <chandrasekhar.pcp@gmail.com>
2021-10-30 09:45:46 +0530

    noty update/controllers update

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,7 +2,7 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(serr, post){
+    Post.findById(req.body.post, function(err, post){
         if(post){
             Comment.create({
                 content: req.body.content,
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -77,11 +77,13 @@ module.exports.create = function(req, res){
 
 // sign-in and create session for user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged In Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'Logged Out Successfully');
 
     return res.redirect('/');
 }
